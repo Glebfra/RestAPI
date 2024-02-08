928a650eb9f4f7036453acc790e48499da1df4dd
glebfra <gleb.frantsuzov@yandex.ru>
2024-02-09 02:45:02 +0300

    = renamed words app to dictionary app

--- api/api/urls.py
+++ api/api/urls.py
@@ -21,7 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dictionary/', include('words.urls'))
+    path('dictionary/', include('dictionary.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- api/dictionary/apps.py
+++ api/dictionary/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WordsConfig(AppConfig):
+class DictionaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'words'
+    name = 'dictionary'
