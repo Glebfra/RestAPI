bd3373dff6586362ca0dd4272840d752a587fbba
glebfra <gleb.frantsuzov@yandex.ru>
2024-03-01 14:27:58 +0300

    + serializers for science app in django + viewsets for science app in django + register them in urls

--- api/science/urls.py
+++ api/science/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import APIElementsViewSet, APIPhasesViewSet, APISaturationsViewSet, APIStatesViewSet
 
 router = DefaultRouter()
+router.register('elements', APIElementsViewSet)
+router.register('states', APIStatesViewSet)
+router.register('phases', APIPhasesViewSet)
+router.register('saturations', APISaturationsViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- api/science/views.py
+++ api/science/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Element, Phase, Saturation, State
+from .serializers import ElementSerializer, PhaseSerializer, SaturationSerializer, StateSerializer
+
+
+class APIElementsViewSet(ModelViewSet):
+    queryset = Element.objects.all()
+    serializer_class = ElementSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.user = request.user
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class APIStatesViewSet(ModelViewSet):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.user = request.user
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class APIPhasesViewSet(ModelViewSet):
+    queryset = Phase.objects.all()
+    serializer_class = PhaseSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.user = request.user
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class APISaturationsViewSet(ModelViewSet):
+    queryset = Saturation.objects.all()
+    serializer_class = SaturationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.user = request.user
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
